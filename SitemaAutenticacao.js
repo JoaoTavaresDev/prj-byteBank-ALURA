fe7f71dca938b0c9b192cde541005623e38d961c
João Paulo <jpcovinhas0@gmail.com>
2021-10-22 16:11:37 -0300

    Projeto final JS Orientado a objetos

--- SitemaAutenticacao.js
+++ SitemaAutenticacao.js
@@ -1,9 +1,21 @@
 /*
 Ser autenticavel significa ter o metodo "autenticar"
+
+ducky type
 */
 
 export class SitemaAutenticacao {
   static login(autenticavel, senha) {
-    return autenticavel.autenticar(senha);
+    if (SitemaAutenticacao.ehAutenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    return (
+      "autenticar" in autenticavel &&
+      autenticavel.autenticar instanceof Function
+    );
   }
 }
